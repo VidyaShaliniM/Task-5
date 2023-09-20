3b0fc8e54f12cb52b6c955a4d64e061bbe9001ea
VidyaShaliniM <143701542+VidyaShaliniM@users.noreply.github.com>
2023-09-20 16:53:03 +0530

    Update 4_Unique_Char.py

--- 4_Unique_Char.py
+++ 4_Unique_Char.py
@@ -1,9 +1,10 @@
 word = input()
+new_word =word.lower()
 # function to take a string and return a string with only unique characters
 
 
 def unique_char(a):
-    uni_char = set(word)
+    uni_char = set(new_word)
     for i in uni_char:
         return len(uni_char)
 
